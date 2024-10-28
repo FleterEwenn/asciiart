b7f66a9b717181a6c4ec8e97224a68f7200090dc
Ewenn <fleter.ewenn@gmail.com>
2024-10-28 18:47:05 +0100

    asciiart v1

--- asciiart.py
+++ asciiart.py
@@ -1,3 +1,29 @@
-import random
 from PIL import Image
+import random
 
+chemin = input("Entrez le chemin de l'image ")
+image2 = Image.open(chemin)
+image = image2.convert('L')
+image2.show()
+largeur, hauteur = image.size
+ascii_list = []
+for y in range(hauteur):
+	ascii_list = []
+	for x in range(largeur):
+		px = image.getpixel((x, y))
+		if px < 5:
+			ascii_list += [' ']
+		elif px < 50:
+			ascii_list += ['.']
+		elif px < 75:
+			ascii_list += [';']
+		elif px < 150:
+			ascii_list += list(random.choice(['[', ']']))
+		elif px < 200:
+			ascii_list += ['&']
+		elif px < 225:
+			ascii_list += ['@']
+		else:
+			ascii_list += ['#']
+	ascii = " ".join(ascii_list)
+	print(ascii)
