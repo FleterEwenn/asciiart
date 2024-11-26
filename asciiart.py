a323594184fc27d63e2579983b0ad13fae57fc05
Ewenn <fleter.ewenn@gmail.com>
2024-11-26 10:38:36 +0100

    ajout d'une méthode + précise pour obtenir un caractère

--- asciiart.py
+++ asciiart.py
@@ -5,6 +5,9 @@ import random
 chemin = input("Entrez le chemin de l'image ")
 image2 = Image.open(chemin)
 
+# création de la chaîne de caractere pour le niveau de gris du plus clair au plus sombre
+caracteres = '@&#]/?!:;-. '
+
 # convertion de l'image en niveaux de gris
 image = image2.convert('L')
 
@@ -17,30 +20,19 @@ largeur, hauteur = image.size
 # crée une boucle qui traite ligne par ligne, ici y est l'ordonnée
 for y in range(hauteur):
 	# initialise la boucle contenant les caractères de chaque ligne
-	ascii_list = []
+	ascii = ''
 
 	# crée une boucle qui traite colonne par colonne, ici x est l'abscisse
 	# donc le couple (x,y) correspond au coordonnées d'un pixel de l'image
 	for x in range(largeur):
 		# on récupère la couleur en niveau de gris d'un pixel de coordonnées x,y
+		# la valeur est comprise entre 0 et 255 :  noir=0 blanc=255
 		px = image.getpixel((x, y))
-		# traitement de la valeur de la couleur(px) comprise entre 0 et 255 :  noir=0 blanc=255
-		if px < 5:
-			ascii_list += [' ']
-		elif px < 50:
-			ascii_list += ['.']
-		elif px < 75:
-			ascii_list += [';']
-		elif px < 150:
-			ascii_list += list(random.choice(['[', ']']))
-		elif px < 200:
-			ascii_list += ['&']
-		elif px < 225:
-			ascii_list += ['@']
-		else:
-			ascii_list += ['#']
+		
+		# proportinalité pour obtenir le niveau de gris en caractere en fonction du nombre de caracteres
+		nv_gris = px*len(caracteres)/255
+		# Le caractere est ajouter avce un espace pour compenser la différence entre la hauteur et la largeur
+		ascii += ' ' + nv_gris
 
-	# rassemble les éléments de la liste, les caractères de la ligne traitée, en une variable pour les afficher
-	# un espace est ajouter a chaque element pour compenser la différence entre la hauteur et la largeur d'un caractère
-	ascii = " ".join(ascii_list)
+	# affiche la ligne de caractere correspondante a la ligne traitée de l'image
 	print(ascii)
